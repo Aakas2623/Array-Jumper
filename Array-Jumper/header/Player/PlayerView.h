4e62c574ea8ed3e83beb413bc464507596c4b97a
Aakash Singh <aaka241@gmail.com>
2025-01-15 05:50:42 +0530

    added player model and view

---
+++ Array-Jumper/header/Player/PlayerView.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "../../header/UI/UIElement/ImageView.h"
+
+namespace Player
+{
+
+	class PlayerView 
+	{
+	private:
+		UI::UIElement::ImageView* player_image;
+
+		sf::RenderWindow* game_window;
+
+		float player_height;
+		float player_width;
+
+		void initializePlayerImage();
+		void drawPlayer();
+		void loadPlayer();
+		void calculatePlayerDimensions();
+		void updatePlayerPosition();
+		sf::Vector2f calulcatePlayerPosition();
+
+	public:
+		PlayerView();
+		~PlayerView();
+
+		void initialize();
+		void update();
+		void render();
+	};
+	
+
+}
---
+++ Array-Jumper/source/Player/PlayerView.cpp
@@ -0,0 +1,71 @@
+#include "../../header/Player/PlayerView.h"
+#include "../../header/Global/Config.h" 
+#include "../../header/Global/ServiceLocator.h"
+
+using namespace Global; 
+using namespace UI::UIElement;
+
+namespace Player
+{
+	
+	PlayerView::PlayerView()
+	{
+		game_window = nullptr;
+		player_image = new ImageView();
+	
+	}
+
+	PlayerView::~PlayerView() {}
+
+	void PlayerView::initialize()
+	{
+		game_window = ServiceLocator::getInstance()->getGraphicService()->getGameWindow();
+		loadPlayer();
+	}
+
+	void PlayerView::update()
+	{
+		//Yet to implement
+	}
+
+	void PlayerView::render()
+	{
+		//Yet to implement
+	}
+
+	void PlayerView::loadPlayer()
+	{
+		calculatePlayerDimensions();
+		initializePlayerImage();
+	}
+
+	void PlayerView::initializePlayerImage()
+	{
+		player_image->initialize(Config::character_texture_path,
+			player_width,
+			player_height,
+			sf::Vector2f(0, 0));
+	}
+
+	void PlayerView::calculatePlayerDimensions()
+	{
+		player_width = 1000.f;
+		player_height = 1000.f;
+	}
+
+	sf::Vector2f PlayerView::calulcatePlayerPosition()
+	{
+		return sf::Vector2f(0, 0);
+	}
+
+	void PlayerView::updatePlayerPosition()
+	{
+		player_image->setPosition(calulcatePlayerPosition());
+	}
+
+	void PlayerView::drawPlayer()
+	{
+		player_image->render();
+	}
+
+}
